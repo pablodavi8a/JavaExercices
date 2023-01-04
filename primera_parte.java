4015443864c1e16889d544d09ac8c9f3c91b39e7
pablodavi8a <pablodavi8a@gmail.com>
2023-01-04 15:32:16 -0500

    Create primera_parte.java

---
+++ primera_parte.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        coche myCoche = new coche();
+        myCoche.inc_puertas();          // añado una puerta
+
+        System.out.println(myCoche.n_puertas);
+
+    }
+    
+    
+}
